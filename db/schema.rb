c8adaf7df90340b77807233d3d87b0a849a9f234
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-04-30 17:31:49 -0500

    Add scaffold for posts and add root page to routes file.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_222148) do
+ActiveRecord::Schema.define(version: 2020_04_30_222809) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +39,5 @@ ActiveRecord::Schema.define(version: 2020_04_30_222148) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
